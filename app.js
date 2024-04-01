f3c1b1ee62bdb8cc675762ba3fac7ca156635fa7
Ivan Starikov <vanya.st.ov@gmail.com>
2024-04-01 16:26:49 +0300

    Update vercel config

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { Telegraf } from 'telegraf';
-import { getRandomTopic } from './topics.js';
-import { showKeybord } from './keyboards.js'
+import express from "express";
+import * as dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+import { getRandomTopic } from "./topics.js";
+import { showKeybord } from "./keyboards.js";
 
 dotenv.config();
 
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-app.get('/', (res) => {
-  res.send('The bot is running')
-})
+app.get("/", (res) => {
+  res.send("The bot is running");
+});
 
-bot.hears('Get a new topic', ctx => {
+bot.hears("Get a new topic", (ctx) => {
   ctx.reply(getRandomTopic());
 });
 
-bot.hears('Delete a topic', ctx => {
-  ctx.reply('Delete');
+bot.hears("Delete a topic", (ctx) => {
+  ctx.reply("Delete");
 });
 
-bot.start(ctx => {
-  ctx.reply('welcome!', showKeybord());
+bot.start((ctx) => {
+  ctx.reply("welcome!", showKeybord());
 });
 
 bot.launch();
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`),
+);
 
+export default app;
